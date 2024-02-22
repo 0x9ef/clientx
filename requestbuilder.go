8c9740053e894ef3851d76e52428e466d5a17246
0x9ef <0x9ef@tutanota.com>
2024-02-22 21:51:19 +0200

    feat(internal): add support for HEAD, OPTIONS, TRACE, CONNECT methods

--- requestbuilder.go
+++ requestbuilder.go
@@ -50,80 +50,107 @@ func NewRequestBuilder[Req any, Resp any](api *API) *RequestBuilder[Req, Resp] {
 }
 
 // WithForm sets the form data for the request.
-func (r *RequestBuilder[Req, Resp]) WithForm(obj url.Values) *RequestBuilder[Req, Resp] {
-	r.requestOptions = append(r.requestOptions, WithRequestForm(obj))
-	return r
+func (rb *RequestBuilder[Req, Resp]) WithForm(obj url.Values) *RequestBuilder[Req, Resp] {
+	rb.requestOptions = append(rb.requestOptions, WithRequestForm(obj))
+	return rb
 }
 
 // WithQueryParams sets URL query parameters from structure by accesing field with provided tag alias.
-func (r *RequestBuilder[Req, Resp]) WithQueryParams(tag string, params ...Req) *RequestBuilder[Req, Resp] {
-	r.requestOptions = append(r.requestOptions, WithRequestQueryParams(tag, params...))
-	return r
+func (rb *RequestBuilder[Req, Resp]) WithQueryParams(tag string, params ...Req) *RequestBuilder[Req, Resp] {
+	rb.requestOptions = append(rb.requestOptions, WithRequestQueryParams(tag, params...))
+	return rb
 }
 
 // WithEncodableQueryParams sets URL query parameters from structure which implements ParamEncoder interface.
-func (r *RequestBuilder[Req, Resp]) WithEncodableQueryParams(params ...ParamEncoder[Req]) *RequestBuilder[Req, Resp] {
-	r.requestOptions = append(r.requestOptions, WithRequestQueryEncodableParams(params...))
-	return r
+func (rb *RequestBuilder[Req, Resp]) WithEncodableQueryParams(params ...ParamEncoder[Req]) *RequestBuilder[Req, Resp] {
+	rb.requestOptions = append(rb.requestOptions, WithRequestQueryEncodableParams(params...))
+	return rb
 }
 
 // AfterResponse adds to a chain function that will be executed after response is obtained.
-func (r *RequestBuilder[Req, Resp]) AfterResponse(f func(resp *http.Response, decoded *Resp) error) *RequestBuilder[Req, Resp] {
-	r.client.afterResponse = append(r.client.afterResponse, f)
-	return r
+func (rb *RequestBuilder[Req, Resp]) AfterResponse(f func(resp *http.Response, decoded *Resp) error) *RequestBuilder[Req, Resp] {
+	rb.client.afterResponse = append(rb.client.afterResponse, f)
+	return rb
 }
 
 // Get builds GET request with no body specified.
 // Appends request options (includes request options that were specified at NewRequestBuilder).
-func (r *RequestBuilder[Req, Resp]) Get(path string, opts ...RequestOption) *RequestBuilder[Req, Resp] {
-	r.method = http.MethodGet
-	r.resourcePath = path
-	r.requestOptions = append(r.requestOptions, opts...)
-	return r
+func (rb *RequestBuilder[Req, Resp]) Get(path string, opts ...RequestOption) *RequestBuilder[Req, Resp] {
+	rb.method = http.MethodGet
+	rb.resourcePath = path
+	rb.requestOptions = append(rb.requestOptions, opts...)
+	return rb
 }
 
 // Post builds POST request with specified body. Appends request options.
-func (r *RequestBuilder[Req, Resp]) Post(path string, body *Req, opts ...RequestOption) *RequestBuilder[Req, Resp] {
-	r.method = http.MethodPost
-	r.resourcePath = path
-	r.body = body
-	r.requestOptions = append(r.requestOptions, opts...)
-	return r
+func (rb *RequestBuilder[Req, Resp]) Post(path string, body *Req, opts ...RequestOption) *RequestBuilder[Req, Resp] {
+	rb.method = http.MethodPost
+	rb.resourcePath = path
+	rb.body = body
+	rb.requestOptions = append(rb.requestOptions, opts...)
+	return rb
 }
 
 // Patch builds PATCH request with specified body (if any). Appends request options.
-func (r *RequestBuilder[Req, Resp]) Patch(path string, body *Req, opts ...RequestOption) *RequestBuilder[Req, Resp] {
-	r.method = http.MethodPatch
-	r.resourcePath = path
-	r.requestOptions = append(r.requestOptions, opts...)
-	r.body = body
-	return r
+func (rb *RequestBuilder[Req, Resp]) Patch(path string, body *Req, opts ...RequestOption) *RequestBuilder[Req, Resp] {
+	rb.method = http.MethodPatch
+	rb.resourcePath = path
+	rb.requestOptions = append(rb.requestOptions, opts...)
+	rb.body = body
+	return rb
 }
 
 // Put builds PUT request with specified body (if any). Appends request options.
-func (r *RequestBuilder[Req, Resp]) Put(path string, body *Req, opts ...RequestOption) *RequestBuilder[Req, Resp] {
-	r.method = http.MethodPut
-	r.resourcePath = path
-	r.requestOptions = append(r.requestOptions, opts...)
-	r.body = body
-	return r
+func (rb *RequestBuilder[Req, Resp]) Put(path string, body *Req, opts ...RequestOption) *RequestBuilder[Req, Resp] {
+	rb.method = http.MethodPut
+	rb.resourcePath = path
+	rb.requestOptions = append(rb.requestOptions, opts...)
+	rb.body = body
+	return rb
 }
 
-// DELETE builds DELETE request with specified body (if any). Appends request options.
-func (r *RequestBuilder[Req, Resp]) Delete(path string, body *Req, opts ...RequestOption) *RequestBuilder[Req, Resp] {
-	r.method = http.MethodDelete
-	r.resourcePath = path
-	r.requestOptions = append(r.requestOptions, opts...)
-	r.body = body
-	return r
+// Delete builds DELETE request with specified body (if any). Appends request options.
+func (rb *RequestBuilder[Req, Resp]) Delete(path string, body *Req, opts ...RequestOption) *RequestBuilder[Req, Resp] {
+	rb.method = http.MethodDelete
+	rb.resourcePath = path
+	rb.requestOptions = append(rb.requestOptions, opts...)
+	rb.body = body
+	return rb
+}
+
+// Head builds HEAD request. Appends request options.
+func (rb *RequestBuilder[Req, Resp]) Head(path string, opts ...RequestOption) *RequestBuilder[Req, Resp] {
+	rb.method = http.MethodHead
+	rb.resourcePath = path
+	rb.requestOptions = append(rb.requestOptions, opts...)
+	return rb
+}
+
+// Trace builds TRACE request. Appends request options.
+func (rb *RequestBuilder[Req, Resp]) Trace(path string, opts ...RequestOption) *RequestBuilder[Req, Resp] {
+	rb.method = http.MethodTrace
+	rb.resourcePath = path
+	rb.requestOptions = append(rb.requestOptions, opts...)
+	return rb
+}
+
+// Options builds OPTIONS request. Appends request options.
+func (rb *RequestBuilder[Req, Resp]) Options(path string, opts ...RequestOption) *RequestBuilder[Req, Resp] {
+	rb.method = http.MethodOptions
+	rb.resourcePath = path
+	rb.requestOptions = append(rb.requestOptions, opts...)
+	return rb
+}
+
+// Connect builds CONNECT request. Appends request options.
+func (rb *RequestBuilder[Req, Resp]) Connect(path string, opts ...RequestOption) *RequestBuilder[Req, Resp] {
+	rb.method = http.MethodConnect
+	rb.resourcePath = path
+	rb.requestOptions = append(rb.requestOptions, opts...)
+	return rb
 }
 
 // Do executes request and decodes response into Resp object, returns error if any.
-func (r *RequestBuilder[Req, Resp]) Do(ctx context.Context) (*Resp, error) {
-	_, data, err := r.client.do(ctx, r)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
+func (rb *RequestBuilder[Req, Resp]) Do(ctx context.Context) (*Resp, error) {
+	return rb.client.do(ctx, rb)
 }
